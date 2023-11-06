c7571be6974920cdf9f78e1fb82a4d348224377c
Adeval Neto Cordeiro de Albuquerque <127787201+AdevalNetoC@users.noreply.github.com>
2023-11-06 17:51:51 -0300

    Update Ingrediente.java

--- objetos/Ingrediente.java
+++ objetos/Ingrediente.java
@@ -1,34 +1,34 @@
-package objetos;
+package pizzaria;
 
 public class Ingrediente {
-  private String nome;
-  private int vezesUsado;
+	  private String nome;
+	  private int vezesUsado;
 
-  public Ingrediente(String nome) {
-    this.nome = nome;
-    this.vezesUsado = 0;
-  }
+	  public Ingrediente(String nome) {
+	    this.nome = nome;
+	    this.vezesUsado = 0;
+	  }
 
-  public String getNome() {
-    return this.nome;
-  }
+	  public String getNome() {
+	    return this.nome;
+	  }
 
-  public void usarIngrediente() {
-    this.vezesUsado++;
-  }
+	  public void usarIngrediente() {
+	    this.vezesUsado++;
+	  }
 
-  public void cancelarUsoDoIngrediente() {
-    this.vezesUsado--;
-  }
+	  public void cancelarUsoDoIngrediente() {
+	    this.vezesUsado--;
+	  }
 
-  @Override
-  public boolean equals(Object outroIngrediente) {
-    Ingrediente convertido = (Ingrediente) outroIngrediente;
-    return nome.equals(convertido.nome);
-  }
+	  @Override
+	  public boolean equals(Object outroIngrediente) {
+	    Ingrediente convertido = (Ingrediente) outroIngrediente;
+	    return nome.equals(convertido.nome);
+	  }
 
-  @Override
-  public String toString() {
-    return "Ingrediente: " + this.nome + ", vezes usado: " + this.vezesUsado;
-  }
-}
+	  @Override
+	  public String toString() {
+	    return "Ingrediente: " + this.nome + ", vezes usado: " + this.vezesUsado;
+	  }
+	}
