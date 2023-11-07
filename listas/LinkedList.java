6f1efe1755b77aaedb7399dedf1083e5d2395333
adnanbezerra <adnanbezerra@proton.me>
2023-11-07 15:18:33 -0300

    adiciona removeLast em linkedlist e mais coisas

--- listas/LinkedList.java
+++ listas/LinkedList.java
@@ -7,53 +7,79 @@ public class LinkedList<E> {
 	private int quantidade = 0;
 		
 	public void addFirst(E valor) {
-			Node<E> novoNode = new Node<E>(valor);
-			novoNode.proximo = cabeca;
-			cabeca = novoNode;
-			if (size() == 0) {
-				cauda = novoNode;
-			}
-			quantidade++;
-		}
-		
-	public void addLast(E valor) {
-			Node<E> novoNode = new Node<E>(valor);
-			if (isEmpty()) {
-				addFirst(valor);
-				return;
-			}
-			cauda.proximo = novoNode;
-			quantidade++;
+		Node<E> novoNode = new Node<E>(valor);
+		novoNode.proximo = cabeca;
+		cabeca = novoNode;
+
+		if (size() == 0) {
 			cauda = novoNode;
 		}
+
+		quantidade++;
+	}
+		
+	public void addLast(E valor) {
+		Node<E> novoNode = new Node<E>(valor);
+		if (isEmpty()) {
+			addFirst(valor);
+			return;
+		}
+
+		cauda.proximo = novoNode;
+		quantidade++;
+		cauda = novoNode;
+	}
 		
 	public E removeFirst() {
-			Node<E> temp = cabeca;
-			cabeca = temp.proximo;
-			quantidade--;
-			return temp.valor;
+		Node<E> temp = cabeca;
+		cabeca = temp.proximo;
+		quantidade--;
+
+		return temp.valor;
+	}
+
+	public E removeLast() {
+    if (cabeca == null) {
+      return null; 
 		}
+  
+    if (cabeca.proximo == null) { 
+      return null; 
+    } 
+  
+    Node<E> second_last = cabeca; 
+    while (second_last.proximo.proximo != null) {
+      second_last = second_last.proximo; 
+		}
+  
+    second_last.proximo = null; 
+  
+    return cabeca.valor;
+	}
 		
 	public int size() {
-			return quantidade;
-		}
+		return quantidade;
+	}
 		
 	public boolean isEmpty() {
-			return quantidade == 0;
-		}
+		return quantidade == 0;
+	}
 		
 	public E first() {
-			return cabeca.valor;
-		}
-		
+		return cabeca.valor;
 	}
 
-	class Node<E> {
-		public E valor;
-		public Node<E> proximo = null;
-		
-		public Node(E valor) {
-			this.valor = valor;
-		}
+	public E getLast() {
+		return cauda.valor;
 	}
+		
+}
 
+class Node<E> {
+	public E valor;
+	public Node<E> proximo = null;
+		
+	public Node(E valor) {
+		this.valor = valor;
+	}
+}
