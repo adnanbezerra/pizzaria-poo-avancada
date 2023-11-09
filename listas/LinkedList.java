785b7f3dfdb3446df8406a2345964dec99a304ec
Natalia <nataliagaldinob@gmail.com>
2023-11-09 11:16:36 -0300

    atualizado

--- listas/LinkedList.java
+++ listas/LinkedList.java
@@ -1,110 +1,138 @@
 package listas;
 
+import objetos.Pedido;
+
 public class LinkedList<Ingrediente> {
-		
-	private Node<Ingrediente> cabeca;
-	private Node<Ingrediente> cauda;
-	private int quantidade = 0;
-		
-	public void addFirst(Ingrediente valor) {
-		Node<Ingrediente> novoNode = new Node<Ingrediente>(valor);
-		novoNode.proximo = cabeca;
-		cabeca = novoNode;
+    private Node<Ingrediente> cabeca;
+    private Node<Ingrediente> cauda;
+    private int quantidade = 0;
 
-		if (size() == 0) {
-			cauda = novoNode;
-		}
+    public void addFirst(Ingrediente valor) {
+        Node<Ingrediente> novoNode = new Node<Ingrediente>(valor);
+        novoNode.proximo = cabeca;
+        cabeca = novoNode;
 
-		quantidade++;
-	}
-		
-	public void addLast(Ingrediente valor) {
-		Node<Ingrediente> novoNode = new Node<Ingrediente>(valor);
-		if (isEmpty()) {
-			addFirst(valor);
-			return;
-		}
+        if (size() == 0) {
+            cauda = novoNode;
+        }
 
-		cauda.proximo = novoNode;
-		quantidade++;
-		cauda = novoNode;
-	}
-		
-	public Ingrediente removeFirst() {
-		Node<Ingrediente> temp = cabeca;
-		cabeca = temp.proximo;
-		quantidade--;
+        quantidade++;
+    }
 
-		return temp.valor;
-	}
+    public void addLast(Ingrediente valor) {
+        Node<Ingrediente> novoNode = new Node<Ingrediente>(valor);
+        if (isEmpty()) {
+            addFirst(valor);
+            return;
+        }
 
-	public Ingrediente removeLast() {
-    if (cabeca == null) {
-      return null; 
-		}
-  
-    if (cabeca.proximo == null) { 
-      return null; 
-    } 
-  
-    Node<Ingrediente> second_last = cabeca; 
-    while (second_last.proximo.proximo != null) {
-      second_last = second_last.proximo; 
-		}
-  
-    second_last.proximo = null; 
-  
-    return cabeca.valor;
-	}
-		
-	public int size() {
-		return quantidade;
-	}
-		
-	public boolean isEmpty() {
-		return quantidade == 0;
-	}
-		
-	public Ingrediente first() {
-		return cabeca.valor;
-	}
+        cauda.proximo = novoNode;
+        quantidade++;
+        cauda = novoNode;
+    }
 
-	public Ingrediente getLast() {
-		return cauda.valor;
-	}
-	
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
+    public Ingrediente removeFirst() {
+        Node<Ingrediente> temp = cabeca;
+        cabeca = temp.proximo;
+        quantidade--;
 
-		if (cabeca == null) {
-      return "Sem ingredientes"; 
-		}
-  
-    if (cabeca.proximo == null) { 
-      builder.append(cabeca.valor); 
-    } 
-  
-    Node<Ingrediente> temp = cabeca; 
-    while (temp.proximo != null) {
-			if (temp.proximo == null) {
-      	builder.append(((objetos.Ingrediente) temp.valor).getNome());
-			} else {
-				builder.append(", " + ((objetos.Ingrediente) temp.valor).getNome());
-			}
+        return temp.valor;
+    }
 
-		}
+    public Ingrediente removeLast() {
+        if (cabeca == null) {
+            return null;
+        }
 
-		return builder.toString();
-	}
+        if (cabeca.proximo == null) {
+            return null;
+        }
+
+        Node<Ingrediente> second_last = cabeca;
+        while (second_last.proximo.proximo != null) {
+            second_last = second_last.proximo;
+        }
+
+        second_last.proximo = null;
+
+        return cauda.valor;
+    }
+
+    public int size() {
+        return quantidade;
+    }
+
+    public boolean isEmpty() {
+        return quantidade == 0;
+    }
+
+    public Ingrediente first() {
+        return cabeca.valor;
+    }
+
+    public Ingrediente getLast() {
+        return cauda.valor;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        if (cabeca == null) {
+            return "Sem ingredientes";
+        }
+
+        if (cabeca.proximo == null) {
+            builder.append(cabeca.valor);
+        }
+
+        Node<Ingrediente> temp = cabeca;
+        while (temp.proximo != null) {
+            if (temp.proximo == null) {
+                builder.append(((objetos.Ingrediente) temp.valor).getNome());
+            } else {
+                builder.append(", " + ((objetos.Ingrediente) temp.valor).getNome());
+            }
+        }
+
+        return builder.toString();
+    }
+    public Node<Ingrediente> getCabeca() {
+        return cabeca;
+    }
+
+		public Ingrediente get(int index) {
+        if (index < 0 || index >= size()) {
+            return null; 
+        }
+
+        Node<Ingrediente> currentNode = cabeca;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.proximo;
+        }
+
+        return currentNode.valor;
+    }
 
 }
 
 class Node<Ingrediente> {
-	public Ingrediente valor;
-	public Node<Ingrediente> proximo = null;
-		
-	public Node(Ingrediente valor) {
-		this.valor = valor;
-	}
+    public Ingrediente valor;
+    public Node<Ingrediente> proximo = null;
+
+    public Node(Ingrediente valor) {
+        this.valor = valor;
+    }
+
+    public Node<Ingrediente> getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(Node<Ingrediente> proximo) {
+        this.proximo = proximo;
+    }
+
+    public Ingrediente getValor() {
+        return valor;
+    }
 }
