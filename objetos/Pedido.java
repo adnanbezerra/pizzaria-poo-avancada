daa3fb8ba3a4afa1def0f105436bc57b326c8229
Natalia <nataliagaldinob@gmail.com>
2023-11-09 11:11:57 -0300

    atualizado

--- objetos/Pedido.java
+++ objetos/Pedido.java
@@ -1,32 +1,38 @@
 package objetos;
 
-public class Pedido {
-	  private Pizza pizza;
-	  private int numeroDaMesa;
+public class Pedido implements Comparable<Pedido> {
+    private Pizza pizza;
+    private int numeroDaMesa;
+    private static int contadorPedidos = 0;
 
-	  public Pedido(Pizza pizza, int numeroDaMesa) {
-	    this.pizza = pizza;
-	    this.numeroDaMesa = numeroDaMesa;
-	  }
+    public Pedido(Pizza pizza, int numeroDaMesa) {
+        this.pizza = pizza;
+        this.numeroDaMesa = numeroDaMesa;
+    }
 
-	  public Pizza getPizza() {
-	    return pizza;
-	  }
+    public Pizza getPizza() {
+        return pizza;
+    }
 
-	  public void setPizza(Pizza pizza) {
-	    this.pizza = pizza;
-	  }
+    public void setPizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
 
-	  public int getNumeroDaMesa() {
-	    return numeroDaMesa;
-	  }
+    public int getNumeroDaMesa() {
+        return numeroDaMesa;
+    }
 
-	  public void setNumeroDaMesa(int numeroDaMesa) {
-	    this.numeroDaMesa = numeroDaMesa;
-	  }
+    public void setNumeroDaMesa(int numeroDaMesa) {
+        this.numeroDaMesa = numeroDaMesa;
+    }
 
-	  @Override
-	  public String toString() {
-	    return "Número da mesa: " + this.numeroDaMesa + ", Pizza: " + this.pizza.toString();
-	  }
-	}
+    @Override
+    public String toString() {
+        return "Número da mesa: " + this.numeroDaMesa + ", Pizza: " + this.pizza.toString();
+    }
+
+    @Override
+    public int compareTo(Pedido outroPedido) {
+    	return this.contadorPedidos - outroPedido.contadorPedidos;
+    }
+}
